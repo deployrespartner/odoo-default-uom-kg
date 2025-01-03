7d8ff5b2b8ce8f2c98a01541417c7e5b8119aaf7
deployrespartner <hello@respartner.fr>
2025-01-03 14:39:34 +0100

    MAJ - Surcharge de _get_default_uom_id en fonction du type de produit

--- models/product_template.py
+++ models/product_template.py
@@ -3,24 +3,28 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    @api.model
-    def default_get(self, fields_list):
-        # Récupérer les valeurs par défaut standard
-        defaults = super().default_get(fields_list)
-        
-        # Si uom_id est dans la liste des champs demandés
-        if 'uom_id' in fields_list:
-            # Définir l'unité de mesure par défaut à kg (ID: 12)
-            defaults['uom_id'] = 3
-            
-            # Si uom_po_id est aussi dans la liste, on le met également à kg
-            if 'uom_po_id' in fields_list:
-                defaults['uom_po_id'] = 3
-                
-        return defaults
+    def _get_default_uom_id(self):
+        # Si le contexte indique que c'est un service, on utilise l'unité
+        if self.env.context.get('default_detailed_type') == 'service':
+            return self.env.ref('uom.product_uom_unit')
+        # Pour les produits stockables et consommables, on utilise kg
+        return self.env.ref('uom.product_uom_kgm')
+
+    # Surcharge du champ pour utiliser notre nouvelle méthode par défaut
+    uom_id = fields.Many2one(
+        'uom.uom',
+        'Unit of Measure',
+        default=_get_default_uom_id,
+        required=True,
+        help="Default unit of measure used for all stock operations."
+    )
 
     @api.onchange('detailed_type')
     def _onchange_detailed_type(self):
-        # Surcharge de la méthode pour empêcher la modification de l'UoM
-        # Ne rien faire pour conserver l'UoM actuelle
-        return
+        # On reprend la logique d'origine pour les services
+        if self.detailed_type == 'service':
+            self.invoice_policy = 'order'
+            self.uom_id = self.uom_po_id = self.env.ref('uom.product_uom_unit')
+        # Pour les consommables et stockables, on force en kg
+        elif self.detailed_type in ['consu', 'product']:
+            self.uom_id = self.uom_po_id = self.env.ref('uom.product_uom_kgm')
