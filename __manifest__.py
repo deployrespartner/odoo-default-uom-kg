4a85311c7bcf7f4a4d61adfd2ae175c12251d9f7
deployrespartner <hello@respartner.fr>
2025-01-03 16:25:54 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -9,8 +9,8 @@ Default UoM Kilogram
 This module sets the default Unit of Measure to Kilogram (kg) for all products,
 regardless of their detailed_type value.
     ''',
-    'author': 'Your Company',
-    'website': 'https://www.yourcompany.com',
+    'author': 'RES PARTNER',
+    'website': 'https://www.respartner.fr',
     'depends': ['product'],
     'data': [],
     'installable': True,
